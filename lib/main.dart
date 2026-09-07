24e9e4552d327769d42f8014745e4cafca5973e3
Saravanan M <romansaravanan143@gmail.com>
2026-09-07 16:12:09 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
 
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const FieldServiceApp());
+}
+
+class FieldServiceApp extends StatelessWidget {
+  const FieldServiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Task',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        useMaterial3: true,
+        inputDecorationTheme: const InputDecorationTheme(
+          border: OutlineInputBorder(),
+        ),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
